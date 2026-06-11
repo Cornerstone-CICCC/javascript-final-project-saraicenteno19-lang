0d70262e6bdebdbefc92c17d4710ee206c5dc719
Sarai <sarai@macbookodesarai.home>
2026-06-11 13:23:55 -0400

    Fix exercise 02

--- src/ex02.js
+++ src/ex02.js
@@ -4,7 +4,22 @@
  * @returns {number}
  */
 function sumArray(arr) {
-  return arr.reduce((total, number) => total + number);
+  if (arr === undefined) {
+    throw new Error('Array cannot be undefined');
+
+  }
+  if(!Array.isArray(arr)) {
+    throw new Error('Argument must be an array');
+
+  }
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'number')
+      throw new Error('Array can only contain numbers');
+
+    total += arr[i];
+  }
+  return total;
 }
 
 module.exports = sumArray;
