bf8b7c5ab84879b235edb77b547fe9e3d67b09c8
Sarai <sarai@MacBook-Neo-de-Sarai.local>
2026-06-12 13:45:10 -0400

    Fix exercise 12

--- src/ex12.js
+++ src/ex12.js
@@ -5,10 +5,39 @@
  * @returns {{characters: number, words: number, sentences: number}}
  */
 function analyzeText(str) {
+  if (str === undefined) {
+    throw new Error('String cannot be undefined');
+  }
+
+  if (typeof str !== 'string') {
+    throw new Error('Argument must be a string');
+  }
+
+  const characters = str.length;
+  const cleanText = str.trim();
+
+  if (cleanText === '') {
+    return {
+      characters: characters,
+      words: 0,
+      sentences: 0,
+    };
+  }
+
+  const words = cleanText.split(/\s+/).length;
+  const punctuation = cleanText.match(/[.!?]/g);
+  let sentences = 0;
+
+  if (punctuation) {
+    sentences = punctuation.length;
+  } else {
+    sentences = 1;
+  }
+
   return {
-    characters: str.length,
-    words: str.split(' ').length,
-    sentences: str.split('.').length - 1,
+    characters: characters,
+    words: words,
+    sentences: sentences,
   };
 }
 
