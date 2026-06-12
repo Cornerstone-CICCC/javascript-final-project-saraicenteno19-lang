afc4b3aab807871a223f28d388c2c2974df201f8
Sarai <sarai@MacBook-Neo-de-Sarai.local>
2026-06-12 12:33:40 -0400

    Fix exercise 05

--- src/ex05.js
+++ src/ex05.js
@@ -4,7 +4,19 @@
  * @returns {number[]}
  */
 function filterEvenNumbers(arr) {
-  return arr.filter((number) => number % 2 === 0);
-}
+  if (arr === undefined) {
+    throw new Error('Array cannot be undefined');
 
+  }
+  if (!Array.isArray(arr)) {
+    throw new Error('Argument must be an array');
+
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'number') {
+      throw new Error('Array can only contain numbers');
+    }
+  }
+  return arr.filter(num => num % 2 === 0);
+} 
 module.exports = filterEvenNumbers;
