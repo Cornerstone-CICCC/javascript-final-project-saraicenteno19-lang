224379fcb9f154c86ba04b221f34c886f8bb4111
Sarai <sarai@MacBook-Neo-de-Sarai.local>
2026-06-12 12:58:44 -0400

    Fix exercise 08

--- src/ex08.js
+++ src/ex08.js
@@ -6,7 +6,25 @@
  * @returns {string[]}
  */
 function search(arr, keyword) {
-  return arr.filter((item) => item.includes(keyword));
+  if (arr === undefined) {
+    throw new Error('Array cannot be undefined');
+  }
+  if (!Array.isArray(arr)) {
+    throw new Error('Argument must be an array');
+  }
+  if (keyword === undefined) {
+    throw new Error('Keyword cannot be undefined');
+  }
+  if (typeof keyword !== 'string') {
+    throw new Error('Keyword must be a string');
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'string') {
+      throw new Error('Array can only contain strings');
+    }
+  }
+  return arr.filter((item) => 
+  item.toLowerCase().includes(keyword.toLowerCase())
+);
 }
-
 module.exports = search;
