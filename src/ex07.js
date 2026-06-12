d393810044bb257ce0d00810a119fc05b3caad1f
Sarai <sarai@MacBook-Neo-de-Sarai.local>
2026-06-12 12:51:13 -0400

    Fix exercise 07

--- src/ex07.js
+++ src/ex07.js
@@ -4,11 +4,26 @@
  * @returns {{positive: number[], negative: number[], zero: number[]}}
  */
 function classifyNumbers(arr) {
+  if (arr === undefined) {
+    throw new Error('Array cannot be undefined');
+
+  }
+  if (!Array.isArray(arr)) {
+    throw new Error('Argument must be an array');
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'number') {
+      throw new Error('Array can only contain numbers');
+
+    }
+
+  }
   return {
-    positive: arr.filter((number) => number > 0),
-    negative: arr.filter((number) => number < 0),
-    zero: arr.filter((number) => number === 0),
+    positive: arr.filter(num => num > 0),
+    negative: arr.filter(num => num < 0),
+    zero: arr.filter(num => num === 0)
   };
-}
+
+} 
 
 module.exports = classifyNumbers;
